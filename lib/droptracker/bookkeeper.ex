57dd6cf4f0e35bd733fa98f605daf08e8155af56
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-12-30 20:46:34 +0100

    Fix broken sync when first user

--- lib/droptracker/bookkeeper.ex
+++ lib/droptracker/bookkeeper.ex
@@ -14,6 +14,9 @@ defmodule Droptracker.Bookkeeper do
 
     Enum.each(users_in_room, &(send(&1, {:add_drop, drop, quantity})))
 
+    IO.inspect(drop)
+    spawn(fn() -> send_price(users_in_room, Map.get(drop, "id")) end)
+
     new_state = Map.update(state, room, [{drop, quantity}], &([{drop, quantity}] ++ &1))
 
     IO.inspect(new_state)
@@ -22,13 +25,20 @@ defmodule Droptracker.Bookkeeper do
   end
 
   def handle_cast({:sync_new_user, room, from}, state) do
-    drops = Map.get(state, room)
-
-    Enum.each(drops, fn(drop) ->
-      {dropMap, quantity} = drop
-      send(from, {:add_drop, dropMap, quantity})
-    end)
+    case Map.get(state, room) do
+      nil ->
+        :ok
+      drops ->
+        Enum.each(drops, fn(drop) ->
+          {dropMap, quantity} = drop
+          send(from, {:add_drop, dropMap, quantity})
+        end)
+    end
 
     {:noreply, state}
+  end
+
+  defp send_price(users, id) do
+    :ok
   end
 end
