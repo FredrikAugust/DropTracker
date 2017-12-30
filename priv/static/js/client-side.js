29ba8cde3cbaa03918ed1cada53d2aa886b1ad9b
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-12-30 17:40:43 +0100

    Add items to the list of drops [No mouse interaction required]

--- priv/static/js/client-side.js
+++ priv/static/js/client-side.js
@@ -2,6 +2,12 @@
 
 // Setup autocomplete
 const nameInput = document.querySelector('.name>input');
+const quantityInput = document.querySelector('.quantity>input');
+const dropsEl = document.querySelector('.drops');
+
+// This will track the state, so we can add it later
+let selectedItem = undefined;
+let drops = [];
 
 const fuseOptions = {
   shouldSort: true,
@@ -24,10 +30,45 @@ const options = {
   source: (term, suggest) => (
     suggest(fuse.search(term).map(i => i.name))
   ),
-  onSelect: (event, term, item) => {
-    // term = the item we want to use
-    alert(term);
+  onSelect: (_event, term, _item) => {
+    selectedItem = term;
+    quantityInput.focus();
   }
 };
 
 const itemCompletion = new autoComplete(options);
+
+// Setup callback when the user has selected quantity of item
+
+quantityInput.addEventListener('keydown', e => {
+  e.keyCode == 13 ? addDrop(selectedItem, e.target.value) : 0
+});
+
+// Callback for when the user has added item
+
+function addDrop(item, quantity) {
+  console.log(`Adding ${item} x ${quantity}`);
+
+  // Clear the input fields, and revert to original focus
+  quantityInput.value = '';
+  nameInput.value = '';
+  nameInput.focus();
+
+  // Dispatch a request for price
+
+  let dropEl = document.createElement('div');
+  let nameHeaderEl = document.createElement('h3');
+  nameHeaderEl.appendChild(document.createTextNode(item));
+
+  let quantityEl = document.createElement('p');
+  quantityEl.appendChild(document.createTextNode(`x ${quantity}`));
+
+  dropEl.appendChild(nameHeaderEl);
+  dropEl.appendChild(quantityEl);
+  
+  dropsEl.prepend(dropEl);
+}
+
+function removeDrop(item, quantity) {
+  console.log(`Removing ${item} x ${quantity}`);
+}
